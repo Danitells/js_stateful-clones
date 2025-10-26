7ee0779b743eec76f1429bf2c546aa7e1596b3a7
DariaSalhalova <salhalovadaria@gmail.com>
2025-10-26 14:50:30 +0200

    task resolution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,5 +1,26 @@
 'use strict';
 
+const state1 = {
+  foo: 'bar',
+  bar: 'foo',
+};
+
+const result = transformStateWithClones(state1, [
+  {
+    type: 'addProperties',
+    extraData: { name: 'Jim', hello: 'world' },
+  },
+  {
+    type: 'removeProperties',
+    keysToRemove: ['bar', 'hello'],
+  },
+  {
+    type: 'addProperties',
+    extraData: { another: 'one' },
+  },
+]);
+
+
 /**
  * @param {Object} state
  * @param {Object[]} actions
@@ -7,7 +28,29 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const baseState = {...state}
+  let resultArr = []
+for (const action of actions) {
+  if(action.type ==='addProperties')
+  {
+    let currentState = {...baseState, ...action.extraData };
+    resultArr.push(currentState);
+  }
+  if (action.type ==='removeProperties')
+  {
+    for (const key of action.keysToRemove) {
+      delete baseState[key];
+      resultArr.push(baseState);
+    }
+   
+  }
+  if (action.type ==='clear')
+    {
+    baseState = {}
+    resultArr.push(baseState)
+    }
+}
 }
 
+console.log(result)
 module.exports = transformStateWithClones;
